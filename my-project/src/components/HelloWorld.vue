6d9bf00425f1c0e6086b30340ae30b2a02f71832
Martin <106612709+menkodot@users.noreply.github.com>
2024-01-29 19:31:38 +0100

    test

--- my-project/src/components/HelloWorld.vue
+++ my-project/src/components/HelloWorld.vue
@@ -1,18 +1,19 @@
 <script>
-  export default {
-    props: {
-      msg: String
-    }
+export default {
+  props: {
+    msg: String
   }
+}
 </script>
 
 <style>
-  p {
-    font-weight: bold;
-  }
+p {
+  font-weight: bold;
+}
 </style>
 
 <template>
   <img alt="" src="/assets/fox.jpeg" />
   <p>{{ msg }}</p>
+  <p>Hi</p>
 </template>
