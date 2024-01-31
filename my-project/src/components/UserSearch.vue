9d54b7acc508e1b74b721b45845e3a27e9b6ebb9
Martin <106612709+menkodot@users.noreply.github.com>
2024-01-31 15:04:36 +0100

    added functionality to search field

--- my-project/src/components/UserSearch.vue
+++ my-project/src/components/UserSearch.vue
@@ -1,6 +1,6 @@
 <script>
 const urlPath = 'https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q='
-
+const uniqueUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/'
 import axios from 'axios'
 
 export default {
@@ -12,24 +12,24 @@ export default {
       text: null,
     }
   },
-  //Use watch on the input field to update this.keyWord
-  //Use onClick to call the function which then uses the url + keyWord value to gather data
+
   methods: {
 
     async fetchData() {
       const { data } = await axios.get(urlPath + this.keyWord)
       console.log(data)
-
-      this.data = data
+      const userSearchPath = urlPath + this.keyWord
+      console.log(userSearchPath)
+      let objectID = data.objectIDs[0]
       // console.log(data.objectIDs[this.index])
 
-      // try {
-      //   const { data: info } = await axios.get(uniqueUrl + data.objectIDs[this.index])
-      //   this.data = info
-      //   console.log(this.data.primaryImageSmall)
-      // } catch (error) {
-      //   console.error("Error:", error)
-      // }
+      try {
+        const { data: info } = await axios.get(uniqueUrl + objectID)
+        this.data = info
+        console.log(this.data.primaryImageSmall)
+      } catch (error) {
+        console.error("Error:", error)
+      }
 
     },
 
@@ -57,5 +57,14 @@ export default {
 <template>
   <input type="text" v-model="keyWord" value="Keyword..">
   <input type="button" v-on:click="onClick" value="Search">
-  <p>{{ this.data }}</p>
+
+  <div v-if="this.data === null">
+    <p>Loading...</p>
+  </div>
+
+  <div v-else>
+    <img :src="this.data.primaryImageSmall" alt="Art">
+  </div>
+
+  <!-- <p>{{ this.data }}</p> -->
 </template>
